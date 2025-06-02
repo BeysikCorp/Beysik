d71ae9075065a9e9b5389e31ec0c833ad605d13d
Dre-OS <65882951+Dre-OS@users.noreply.github.com>
2025-06-02 13:34:26 +0800

    Add missing clone command for Beysik-Common repository

--- clone-repos.sh
+++ clone-repos.sh
@@ -21,6 +21,7 @@ clone_or_pull() {
 }
 
 # Replace these with your actual repo URLs
+clone_or_pull https://github.com/Beysik/Beysik-Common.git Beysik-Common
 clone_or_pull https://github.com/Beysik/Beysik-YARP.git Beysik-YARP
 clone_or_pull https://github.com/Beysik/Beysik-UI.git Beysik-UI
 clone_or_pull https://github.com/your-org/Beysik-CartService.git Beysik-CartService
